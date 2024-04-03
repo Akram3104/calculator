8b0edaacfc79c3317da35954d2f81a8b62e7263a
Akram3104 <abdulakram2002@gmail.com>
2024-04-03 22:09:56 +0530

    testing addition
    without addition function

---
+++ test_calculator.py
@@ -0,0 +1,12 @@
+import pytest
+import calculator
+
+# Addition tests
+@pytest.mark.parametrize("num1, num2, expected", [
+    (5, 3, 8),
+    (-5, 3, -2),
+    (0, 0, 0)
+])
+def test_addition(num1, num2, expected):
+    assert calculator.add(num1, num2) == expected
+
