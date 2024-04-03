05bd6ad04bfb797725a4ab2ef663b5e12d237798
Akram3104 <abdulakram2002@gmail.com>
2024-04-03 23:11:36 +0530

    cm

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,5 @@
 import unittest
-from calculator import addition, subtraction
+from calculator import addition, subtraction,multiplication
 
 class TestCalculator(unittest.TestCase):
     def test_addition(self):
@@ -11,6 +11,10 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(subtraction(5, 3), 2)
         self.assertEqual(subtraction(5, -3), 8)
         self.assertEqual(subtraction(0, 0), 0)
+    def test_multiplication(self):
+        self.assertEqual(multiplication(5, 3), 15)
+        self.assertEqual(multiplication(-5, 3), -15)
+        self.assertEqual(multiplication(0, 5), 0)
 
 
 if __name__ == "__main__":
